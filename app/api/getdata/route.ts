7a5cdfa78fb019877cb178e8f32efc3a3a9ad4cc
Charly Fontaine <charly@datadoghq.com>
2023-11-09 10:09:27 -0600

    adding DD tracer, update deps and openai api

--- app/api/getdata/route.ts
+++ app/api/getdata/route.ts
@@ -1,32 +1,22 @@
-import { NextResponse } from 'next/server';
+import {NextResponse}  from 'next/server';
+import OpenAI from "openai";
 
 let answer: string = '';
 
 export async function POST(req: Request) {
-
+    const openai = new OpenAI();
     let counter = 0;
 
     const body = await req.json()
     console.log('body: ', body)
 
     console.log(body.query)
-
-    const response = await fetch('https://api.openai.com/v1/chat/completions', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-            'Authorization': 'Bearer '+process.env.OPENAITOKEN
-        },
-        body: JSON.stringify({
-            model: "gpt-3.5-turbo",
-            messages: [{"role": "user", "content": body.query}],
-            max_tokens: 4000,
-            temperature: 0.5,
-        })
-    });
+    const response = await openai.chat.completions.create({
+        model: "gpt-3.5-turbo",
+        messages: [{"role": "user", "content": body.query}],
+      });
     
-    const json = await response.json();
-    answer = json.choices[0].message.content.replace(/\n\n/, '')
+    const json = response.choices[0];
     console.log("return successful")
 
     return NextResponse.json({ data: `${answer}` })
